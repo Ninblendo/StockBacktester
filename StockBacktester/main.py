535c347ea0bfb4c923eb65658b67624b25f0d73b
David Isaac <davidisaac2100@gmail.com>
2026-08-05 16:53:22 -0700

    added CAGR (annual growth rate)

--- StockBacktester/main.py
+++ StockBacktester/main.py
@@ -48,12 +48,25 @@ strategy_returns = (
 cumMovingAvgReturns = (1 + strategy_returns).cumprod()
 strategy_value = STARTING_CASH * cumMovingAvgReturns
 
+# Calculate the length of the backtest in years
+number_of_days = (close.index[-1] - close.index[0]).days
+number_of_years = number_of_days / 365.25
+
+# Calculate annualized returns
+buy_and_hold_annual_return = (
+    cum_returns.iloc[-1] ** (1 / number_of_years)
+) - 1
+
+moving_average_annual_return = (
+    cumMovingAvgReturns.iloc[-1] ** (1 / number_of_years)
+) - 1
+
 # Display results
 print(f"Buy and hold: ${buy_and_hold.iloc[-1]:,.2f}")
 print(f"Moving-average strategy: ${strategy_value.iloc[-1]:,.2f}")
-print(f"With buy-and-hold strategy, money increased by {cum_returns.iloc[-1]:,.0f}x")
-print(f"With moving-average strategy, money increased by {cumMovingAvgReturns.iloc[-1]:,.0f}x")
-
+print(f"With buy-and-hold strategy, money increased by {cum_returns.iloc[-1]:,.2f}x, and growth was {buy_and_hold_annual_return:.2%} per year")
+print(f"With moving-average strategy, money increased by {cumMovingAvgReturns.iloc[-1]:,.2f}x, and growth was {moving_average_annual_return:.2%} per year")
+# anual growth per year = total growth %/years
  
 pd.DataFrame(
     {
